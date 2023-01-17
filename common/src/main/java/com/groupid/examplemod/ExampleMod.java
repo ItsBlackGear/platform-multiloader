ecd1697a8a6568d4da9e9a58ecad612a4e74a6eb
ItsBlackGear <itsblackgear@gmail.com>
2023-01-17 17:59:28 -0300

    pre-release 1

--- common/src/main/java/com/groupid/examplemod/ExampleMod.java
+++ common/src/main/java/com/groupid/examplemod/ExampleMod.java
@@ -1,8 +1,12 @@
 package com.groupid.examplemod;
 
+import com.groupid.examplemod.platform.ModInstance;
+
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
+    public static final ModInstance INSTANCE = ModInstance.create(MOD_ID).build();
 
     public static void init() {
+        INSTANCE.bootstrap();
     }
 }
